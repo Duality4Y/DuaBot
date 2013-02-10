28262e08684172990db57e56b606c14c52af9bed
Duality4y <201292@live.nl>
2013-02-11 00:48:59 +0100

    modified:   ircBot/BotBrain.py

--- ircBot/BotBrain.py
+++ ircBot/BotBrain.py
@@ -9,7 +9,6 @@ Just to do as little as possible in the main loop.
 """
 
 class BotBrain(irc):
-<<<<<<< HEAD
     found = False
     command = ""
     data = ""
@@ -19,11 +18,6 @@ class BotBrain(irc):
 		#for test purposes we also print.
 		print data+"\r\n"
 		self.log.write(data)
-=======
-    found = False;
-    command = "";
-    data = "";
->>>>>>> 5cf2246b5fc32fb4a67f3474e69d8b4910f8e8e4
     def parseCommand(self,data):
         self.data = data
         self.found = self.findPing(data)
